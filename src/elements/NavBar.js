7c1378d2371d22a6c5724303fa4d488ef9a3cd03
jatin09univest <114483886+jatin09univest@users.noreply.github.com>
2023-05-25 21:02:16 +0530

    bug fixed

--- src/elements/NavBar.js
+++ src/elements/NavBar.js
@@ -14,38 +14,19 @@ export default function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
     const [showSearchBar, setShowSearchBar] = useState(false);
     return (
-        <nav className={`bg-black-50 text-white fixed left-0 right-0 top-3 z-10 bg-repeat ${!isOpen && 'backdrop-blur-sm'}`} >
-            <div className={`font-Inter max-w-screen-xl mx-auto flex items-center lg:justify-between lg:px-8 flex-wrap ${showSearchBar ? ' p-2' : ' p-4'}`}>
+        <nav className={`bg-black-50 text-white fixed left-0 right-0 top-3 z-10`} >
+            <div className={`font-Inter max-w-screen-xl mx-auto flex items-center lg:justify-between lg:px-8 flex-wrap backdrop-blur-sm ${showSearchBar ? ' p-2' : ' p-4'}`}>
                 {showSearchBar ? <SearchBar forPhone={true} setShowSearchBar={setShowSearchBar} />
                     : <>
                         <Link href='/' className="mr-2 inline-flex items-center">
                             <Image src={logoWhite} className='w-24 lg:w-36 object-contain' alt='icon' />
                         </Link>
                         <GoSearch color='#FFFFFF' size={16} onClick={() => setShowSearchBar(true)} className='cursor-pointer h-4 object-contain lg:hidden ml-auto mr-8' />
-                        <Menu right onStateChange={(e) => setIsOpen(e.isOpen)} isOpen={isOpen}
-                            burgerButtonClassName='lg:hidden w-6 h-6 relative overflow-hidden'
-                            customBurgerIcon={<>
-                                <span className='block w-4 h-[2px] rounded bg-white mt-1 ml-1' />
-                                <span className='block w-5 h-[2px] rounded bg-white my-1' />
-                                <span className='block w-4 h-[2px] rounded bg-white ml-1' />
-                            </>}>
-                            <Link href='/stocks' onClick={() => setIsOpen(false)}
-                                className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
-                                Stocks
-                            </Link>
-                            <Link href='/blogs' onClick={() => setIsOpen(false)}
-                                className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
-                                Blogs
-                            </Link>
-                            <Link href='/screeners' onClick={() => setIsOpen(false)}
-                                className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
-                                Screeners
-                            </Link>
-                            <Link href='/elite' onClick={() => setIsOpen(false)}
-                                className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
-                                Elite
-                            </Link>
-                        </Menu>
+                       <div onClick={()=>setIsOpen(true)}>
+                            <span className='block w-4 h-[2px] rounded bg-white mt-1 ml-1' />
+                            <span className='block w-5 h-[2px] rounded bg-white my-1' />
+                            <span className='block w-4 h-[2px] rounded bg-white ml-1' />
+                       </div>
                     </>}
                 <div className="hidden text-base font-semibold lg:flex flex-row items-center w-auto">
                     <Link href='/stocks' className="mx-4 px-3 py-2 hover:hover:opacity-75">
@@ -63,6 +44,24 @@ export default function NavBar() {
                     <SearchBar forPhone={false} />
                 </div>
             </div>
+            <Menu right onStateChange={(e) => setIsOpen(e.isOpen)} isOpen={isOpen} burgerButtonClassName='hidden'>
+                <Link href='/stocks' onClick={() => setIsOpen(false)}
+                    className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
+                    Stocks
+                </Link>
+                <Link href='/blogs' onClick={() => setIsOpen(false)}
+                    className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
+                    Blogs
+                </Link>
+                <Link href='/screeners' onClick={() => setIsOpen(false)}
+                    className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
+                    Screeners
+                </Link>
+                <Link href='/elite' onClick={() => setIsOpen(false)}
+                    className="w-full ml-6 my-2 py-2 rounded items-center justify-center hover:hover:opacity-75">
+                    Elite
+                </Link>
+            </Menu>
         </nav>
-    )
+       )
 }
