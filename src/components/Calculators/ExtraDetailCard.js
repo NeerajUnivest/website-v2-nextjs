4b6373db10716a29704c41a763b96bb638d2600f
NeerajUnivest <neeraj.kadian@univest.in>
2024-01-09 19:47:37 +0530

    bug fixed again

--- src/components/Calculators/ExtraDetailCard.js
+++ src/components/Calculators/ExtraDetailCard.js
@@ -8,11 +8,12 @@ export default function ExtraDetailCard({ heading, content, active, setActive, i
             onClick={() => setActive(active === i ? -1 : i)}>
             <div className='flex flex-row p-3'>
                 {/* <div className='mr-3 mt-1.5 h-1 w-1 lg:h-2 lg:w-2 bg-[#202020] rounded-full' /> */}
-                <p className='w-10/12 font-semibold text-xs lg:text-base'>{heading}</p>
+                <p className='w-10/12 font-semibold text-xs lg:text-bas '>{heading}</p>
                 <Image src={arrowDown} className={`ml-auto mt-1.5 h-2 lg:h-3 w-auto mr-2 ease-in-out duration-300 ${active === i ? 'rotate-180' : 'rotate-0'}`} alt="arrow" />
             </div>
-            <div className={`overflow-hidden ease-in-out duration-300 mx-4 text-justify ml-8
-            ${active === i ? ' opacity-100' : 'max-h-0 opacity-0'}`}>
+            <div className={`overflow-hidden ease-in-out duration-300 mx-4 text-justify ml-3
+            ${active === i ? ' opacity-100' : 'max-h-0 opacity-0 ml-0'}`}>
+                <div className='h-px self-stretch bg-[#EDEDED]' ></div>
                 <div className='mb-4 mt-2 font-normal text-xs lg:text-base text-[#202020]' dangerouslySetInnerHTML={{ __html: content }} />
             </div>
         </div>
