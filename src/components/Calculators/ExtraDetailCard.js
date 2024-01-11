5c59aacb04212553cebaeaea28ac5fb417520714
NeerajUnivest <neeraj.kadian@univest.in>
2024-01-11 16:48:35 +0530

    Extra Details Section Changed

--- src/components/Calculators/ExtraDetailCard.js
+++ src/components/Calculators/ExtraDetailCard.js
@@ -4,18 +4,22 @@ import Image from 'next/image';
 
 export default function ExtraDetailCard({ heading, content, active, setActive, i }) {
     return (
-        <div className={`ease-in-out duration-300 border rounded-xl cursor-pointer bg-white ${active === i ? 'border-black border-1' : 'border-[#dfdfdf]'} `}
-            onClick={() => setActive(active === i ? -1 : i)}>
-            <div className='flex flex-row p-3'>
-                {/* <div className='mr-3 mt-1.5 h-1 w-1 lg:h-2 lg:w-2 bg-[#202020] rounded-full' /> */}
-                <p className='w-10/12 font-bold text-sm lg:text-base '>{heading}</p>
-                <Image src={arrowDown} className={`ml-auto mt-1.5 h-2 lg:h-3 w-auto mr-2 ease-in-out duration-300 ${active === i ? 'rotate-180' : 'rotate-0'}`} alt="arrow" />
-            </div>
-            <div className={`overflow-hidden ease-in-out duration-300 mx-4 text-justify ml-3
-            ${active === i ? ' opacity-100' : 'max-h-0 opacity-0 ml-0'}`}>
-                <div className='h-px self-stretch bg-[#EDEDED]' ></div>
-                <div className='mb-4 mt-2 font-medium text-xs lg:text-sm text-[#202020]' dangerouslySetInnerHTML={{ __html: content }} />
-            </div>
+        // <div className={`ease-in-out duration-300 border rounded-xl cursor-pointer bg-white ${active === i ? 'border-black border-1' : 'border-[#dfdfdf]'} `}
+        //     onClick={() => setActive(active === i ? -1 : i)}>
+        //     <div className='flex flex-row p-3'>
+        //         {/* <div className='mr-3 mt-1.5 h-1 w-1 lg:h-2 lg:w-2 bg-[#202020] rounded-full' /> */}
+        //         <p className='w-10/12 font-bold text-sm lg:text-base '>{heading}</p>
+        //         <Image src={arrowDown} className={`ml-auto mt-1.5 h-2 lg:h-3 w-auto mr-2 ease-in-out duration-300 ${active === i ? 'rotate-180' : 'rotate-0'}`} alt="arrow" />
+        //     </div>
+        //     <div className={`overflow-hidden ease-in-out duration-300 mx-4 text-justify ml-3
+        //     ${active === i ? ' opacity-100' : 'max-h-0 opacity-0 ml-0'}`}>
+        //         <div className='h-px self-stretch bg-[#EDEDED]' ></div>
+        //         <div className='mb-4 mt-2 font-medium text-xs lg:text-sm text-[#202020]' dangerouslySetInnerHTML={{ __html: content }} />
+        //     </div>
+        // </div>
+        <div className='flex flex-col gap-3'>
+            <p className='font-bold text-sm lg:text-base '>{heading}</p>
+            <div className='font-medium text-xs lg:text-sm text-[#202020]' dangerouslySetInnerHTML={{ __html: content }} />
         </div>
     )
 }
--- src/components/Calculators/ExtraDetailsSection.js
+++ src/components/Calculators/ExtraDetailsSection.js
@@ -3,13 +3,14 @@ import ExtraDetailCard from './ExtraDetailCard';
 
 export default function ExtraDetailsSection({ data }) {
 
-    const [active, setActive] = useState(-1);
+    // const [active, setActive] = useState(-1);
 
     return (
-        <section className='flex flex-col items-start gap-3 p-4 bg-[#F5F5F5] lg:rounded-2xl'>
-            <div className='flex flex-col py-8 gap-3 lg:gap-4'>
+        <section className='flex flex-col items-start gap-3 mx-4 lg:mx-0 bg-[#fff] lg:rounded-2xl'>
+            <div className='flex flex-col gap-6'>
                 {data?.map((ele, i) =>
-                    <ExtraDetailCard key={i} heading={ele.question} content={ele.answer} active={active} i={i} setActive={setActive} />
+                    // <ExtraDetailCard key={i} heading={ele.question} content={ele.answer} active={active} i={i} setActive={setActive} />
+                    <ExtraDetailCard key={i} heading={ele.question} content={ele.answer} />
                 )}
             </div>
         </section>
